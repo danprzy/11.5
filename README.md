729b1fe5be8e25f9c79c0259f8e31686766191c2
Danuta Przydrozna <danprzy@gmail.com>
2018-07-09 16:47:15 +0200

    Add readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# 11.5 The 'this' Keyword
+> Learning how to store the context 'var self = this' when a function needs the enclosing context.
--- scripts.js
+++ scripts.js
@@ -1,20 +1,20 @@
 // funkcja tworząca nowe przyciski
-$(function () {
-	function Button(text) {
-		this.text = text || 'Hello'; // jeśli obiektu bez podania argumentu, to ustawiam domyślną wartość
-	}
+$(function() {
+    function Button(text) {
+        this.text = text || 'Hello'; // jeśli obiektu bez podania argumentu, to ustawiam domyślną wartość
+    }
 
-	Button.prototype = {
-		create: function() {
-			var self = this;
-			this.$element = $('<button>');
-			this.$element.text(this.text);
-			this.$element.click(function() {
-				alert(self.text);
-			});
-			$('body').append(this.$element);
-		}
-	}
-	var btn1 = new Button('Hello!');
-	btn1.create();
+    Button.prototype = {
+        create: function() {
+            var self = this;
+            this.$element = $('<button>');
+            this.$element.text(this.text);
+            this.$element.click(function() {
+                alert(self.text);
+            });
+            $('body').append(this.$element);
+        }
+    }
+    var btn1 = new Button('Hello!');
+    btn1.create();
 });
